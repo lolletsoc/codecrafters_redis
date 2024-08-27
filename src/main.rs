027d534e22415be28e50d929ac7d06ebad33a74b
Liam Costello <liam@dehook.io>
2024-08-27 22:12:24 +0100

    Simple PONG

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::io::Write;
 use std::net::TcpListener;
+
+enum Command {
+    PING,
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream
+                    .write("+PONG\r\n".to_owned().as_bytes())
+                    .expect("Failed to send bytes");
             }
             Err(e) => {
                 println!("error: {}", e);
