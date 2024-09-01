63f0991867366b84a7c220790d4d85a99d4ae4b5
Liam Costello <liam@dehook.io>
2024-09-01 22:21:04 +0100

    Get tokio up a running

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,43 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::net::TcpListener;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufStream};
+use tokio::net::TcpListener;
 
 enum Command {
     PING,
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), anyhow::Error> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut command = "".to_owned();
+            let mut buf_stream = BufStream::new(&mut stream);
 
-                let mut command = "".to_owned();
-                let mut buf_reader = BufReader::new(&_stream);
+            loop {
+                let num_bytes = buf_stream
+                    .read_line(&mut command)
+                    .await
+                    .expect("Failed to read from stream");
 
-                let mut buf_writer = BufWriter::new(&_stream);
+                println!("Read {} bytes", num_bytes);
+                println!("Received: {}", command);
 
-                loop {
-                    let num_bytes = buf_reader
-                        .read_line(&mut command)
-                        .expect("Failed to read from stream");
-
-                    println!("Read {} bytes", num_bytes);
-                    println!("Received: {}", command);
-
-                    if num_bytes == 0 {
-                        break;
-                    }
-
-                    if command.ends_with("PING\r\n") {
-                        buf_writer
-                            .write("+PONG\r\n".to_owned().as_bytes())
-                            .expect("Failed to send bytes");
-
-                        buf_writer.flush().unwrap();
-                    }
-                    command.clear();
+                if num_bytes == 0 {
+                    break;
                 }
+
+                if command.ends_with("PING\r\n") {
+                    buf_stream
+                        .write("+PONG\r\n".to_owned().as_bytes())
+                        .await
+                        .expect("Failed to send bytes");
+
+                    buf_stream.flush().await.unwrap();
+                }
+                command.clear();
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
